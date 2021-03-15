65a8220e5b74622f8fa684f94f0061da092d04dd
brad <bradford.sawadye@jembi.org>
2021-03-15 15:33:26 +0200

    fix more lint issues
    DEB-29

--- features/utils.js
+++ features/utils.js
@@ -7,12 +7,13 @@ const OPENHIM_API_HOSTNAME = process.env.OPENHIM_API_HOSTNAME || 'localhost'
 const OPENHIM_TRANSACTION_API_PORT =
   process.env.OPENHIM_TRANSACTION_API_PORT || '5001'
 const OPENHIM_API_PORT = process.env.OPENHIM_API_PORT || '8080'
-const CUSTOM_TOKEN_ID = process.env.CUSTOM_TOKEN_ID || '8c161731-4e72-4446-8950-ce79aadaa75c'
+const CUSTOM_TOKEN_ID =
+  process.env.CUSTOM_TOKEN_ID || '8c161731-4e72-4446-8950-ce79aadaa75c'
 
 const deleteResource = (id, resourceType) => {
   return axios({
-      url: `${OPENHIM_PROTOCOL}://${OPENHIM_API_HOSTNAME}:${OPENHIM_TRANSACTION_API_PORT}/fhir/${resourceType}/${id}`,
-      method: 'DELETE'
+    url: `${OPENHIM_PROTOCOL}://${OPENHIM_API_HOSTNAME}:${OPENHIM_TRANSACTION_API_PORT}/fhir/${resourceType}/${id}`,
+    method: 'DELETE'
   })
 }
 
@@ -34,24 +35,28 @@ exports.verifyOpenhimIsRunning = async () => {
 
   if (response.status !== 200) throw Error('The OpenHim is currently down')
 
-  if (
-    !response.data.mediators ||
-    !Object.keys(response.data.mediators).length
-  ) throw Error('Mediators are currently down')
+  if (!response.data.mediators || !Object.keys(response.data.mediators).length)
+    throw Error('Mediators are currently down')
 }
 
-exports.sendRequest = async (data, path, method = 'POST', token = CUSTOM_TOKEN_ID) => {
+exports.sendRequest = async (
+  data,
+  path,
+  method = 'POST',
+  token = CUSTOM_TOKEN_ID
+) => {
   const response = await axios({
     url: `${OPENHIM_PROTOCOL}://${OPENHIM_API_HOSTNAME}:${OPENHIM_TRANSACTION_API_PORT}/${path}`,
     method: method,
     headers: {
-      "Content-Type": 'application/json',
+      'Content-Type': 'application/json',
       Authorization: `Custom ${token}`
     },
     data: data
   })
 
-  if (response.status != 202) throw Error(`Sending of request on path ${path} failed`)
+  if (response.status != 202)
+    throw Error(`Sending of request on path ${path} failed`)
   return response
 }
 
